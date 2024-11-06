82568ed2dd20aec419a7f3f81a111318703865ba
saylormoonbtc <saylormoonbtc@gmail.com>
2024-11-06 10:54:51 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-
-function copyContractAddress() {
-    const contractAddress = "GrKaJCeHCJ1P7shdiPax9U6VvED9feFo3Yjz5uZspump";
-    navigator.clipboard.writeText(contractAddress).then(() => {
-        alert('Contract address copied!');
-    });
-}
